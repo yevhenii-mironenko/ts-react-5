e6570663e383cf1f1a2090fdd2b89076adc4bd91
Yevhenii Myronenko <jackmironenko@gmail.com>
2026-03-02 21:16:11 +0200

    feat: implement addToCart and removeFromCart reducers in cart slice

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -8,8 +8,29 @@ export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart(state, action: PayloadAction<CartItem>) {},
-    removeFromCart() {},
+    addToCart(
+      state,
+      action: PayloadAction<{ id: string; title: string; price: number }>,
+    ) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id,
+      );
+      if (itemIndex >= 0) {
+        state.items[itemIndex].quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload,
+      );
+      if (state.items[itemIndex].quantity === 1) {
+        state.items.splice(itemIndex, 1);
+      } else {
+        state.items[itemIndex].quantity--;
+      }
+    },
   },
 });
 
